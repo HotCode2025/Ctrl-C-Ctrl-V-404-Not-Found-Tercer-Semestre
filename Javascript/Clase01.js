16ac9ff773994bcee0ef15d62a7aa7c05375ded7
rocio2102 <rocioluceroal@gmail.com>
2026-04-06 22:00:46 -0300

    Clase 01 - Proyecto MundoPC

---
+++ Javascript/Clase01.js
@@ -0,0 +1,173 @@
+class DispositivoEntrada {
+  constructor(tipoEntrada, marca) {
+    this._tipoEntrada = tipoEntrada;
+    this._marca = marca;
+  }
+
+  get tipoEntrada() {
+    return this._tipoEntrada;
+  }
+  set tipoEntrada(tipoEntrada) {
+    this._tipoEntrada = tipoEntrada;
+  }
+
+  get marca() {
+    return this._marca;
+  }
+  set marca(marca) {
+    this._marca = marca;
+  }
+}
+class Raton extends DispositivoEntrada {
+  static contadorRatones = 0;
+
+  constructor(tipoEntrada, marca) {
+    super(tipoEntrada, marca);
+    this._idRaton = ++Raton.contadorRatones;
+  }
+
+  toString() {
+    return `Raton: [idRaton: ${this._idRaton}, tipoEntrada: ${this.tipoEntrada}, marca: ${this.marca}]`;
+  }
+}
+
+class Teclado extends DispositivoEntrada {
+  static contadorTeclados = 0;
+
+  constructor(tipoEntrada, marca) {
+    super(tipoEntrada, marca);
+    this._idTeclado = ++Teclado.contadorTeclados;
+  }
+
+  toString() {
+    return `Teclado: [idTeclado: ${this._idTeclado}, tipoEntrada: ${this.tipoEntrada}, marca: ${this.marca}]`;
+  }
+}
+
+class Monitor {
+  static contadorMonitores = 0;
+
+  constructor(marca, tamaño) {
+    this._idMonitor = ++Monitor.contadorMonitores;
+    this._marca = marca;
+    this._tamaño = tamaño;
+  }
+
+  get idMonitor() {
+    return this._idMonitor;
+  }
+
+  get marca() {
+    return this._marca;
+  }
+  set marca(marca) {
+    this._marca = marca;
+  }
+
+  get tamaño() {
+    return this._tamaño;
+  }
+  set tamaño(tamaño) {
+    this._tamaño = tamaño;
+  }
+
+  toString() {
+    return `Monitor: [idMonitor: ${this._idMonitor}, marca: ${this._marca}, tamaño: ${this._tamaño}]`;
+  }
+}
+
+class Computadora {
+  static contadorComputadoras = 0;
+
+  constructor(nombre, monitor, teclado, raton) {
+    this._idComputadora = ++Computadora.contadorComputadoras;
+    this._nombre = nombre;
+    this._monitor = monitor;
+    this._teclado = teclado;
+    this._raton = raton;
+  }
+
+  get idComputadora() {
+    return this._idComputadora;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+  get monitor() {
+    return this._monitor;
+  }
+  set monitor(monitor) {
+    this._monitor = monitor;
+  }
+  get teclado() {
+    return this._teclado;
+  }
+  set teclado(teclado) {
+    this._teclado = teclado;
+  }
+  get raton() {
+    return this._raton;
+  }
+  set raton(raton) {
+    this._raton = raton;
+  }
+
+  toString() {
+    return `Computadora ${this._idComputadora}: ${this._nombre} \n ${this._monitor} \n ${this._raton} \n ${this._teclado}`;
+  }
+}
+class Orden {
+  static contadorOrdenes = 0;
+
+  constructor() {
+    this._idOrden = ++Orden.contadorOrdenes;
+    this._computadoras = [];
+  }
+
+  get idOrden() {
+    return this._idOrden;
+  }
+
+  agregarComputadora(computadora) {
+    this._computadoras.push(computadora);
+  }
+
+  mostrarOrden() {
+    let computadorasOrden = "";
+    for (let computadora of this._computadoras) {
+      computadorasOrden += `\n${computadora.toString()}`;
+    }
+
+    console.log(`Orden: ${this._idOrden}, Computadoras: ${computadorasOrden}`);
+  }
+}
+
+let raton1 = new Raton("USB", "Logitech");
+let raton2 = new Raton("Bluetooth", "Genius");
+console.log(raton1.toString());
+console.log(raton2.toString());
+
+let teclado1 = new Teclado("USB", "Redragon");
+console.log(teclado1.toString());
+let monitor1 = new Monitor("Samsung", "27 pulgadas");
+let monitor2 = new Monitor("LG", "24 pulgadas");
+console.log(monitor1.toString());
+console.log(monitor2.toString());
+
+let compu1 = new Computadora("HP", monitor1, teclado1, raton1);
+console.log(compu1.toString());
+
+let compu2 = new Computadora("Gamer Armada", monitor2, teclado1, raton2);
+console.log(compu2.toString());
+
+let orden1 = new Orden();
+orden1.agregarComputadora(compu1);
+orden1.agregarComputadora(compu2);
+orden1.mostrarOrden();
+
+let orden2 = new Orden();
+orden2.agregarComputadora(compu2);
+orden2.mostrarOrden();
