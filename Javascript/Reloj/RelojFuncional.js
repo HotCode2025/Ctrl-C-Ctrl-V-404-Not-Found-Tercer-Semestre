7c5532ef74cc73043b7101ec35d2da0c6cb6f5e6
HugoZanatta95 <hugo-zanatta@hotmail.com>
2026-05-11 23:08:34 -0300

    Add files via upload

---
+++ Javascript/Reloj/RelojFuncional.js
@@ -0,0 +1,26 @@
+// Saludo setTimeout
+function miFuncionBatman(){
+    document.getElementById("mensaje").innerHTML += "Ctr C + Ctrl V = 404 Not Found <br>";
+}
+
+setTimeout(miFuncionBatman, 3000);
+
+setTimeout(function() {
+    document.getElementById("mensaje").innerHTML += "Buenas noches Profe <br>";
+}, 5000);
+
+let reloj = () => {
+
+    let fecha = new Date ();
+
+    let horas = fecha.getHours().toString().padStart(2, '0');
+    let minutos = fecha.getMinutes().toString().padStart(2, '0');
+    let segundos = fecha.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById("reloj").innerHTML =
+    `${horas}:${minutos}:${segundos}`;
+}
+
+reloj();
+
+setInterval(reloj, 1000);
