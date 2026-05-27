cf90ebf4cfc367c550ea29b8930cdc783e83a017
Stefano Aymar <ste.aymar@gmail.com>
2026-05-27 18:54:11 -0300

    funcion eleccion enemigo

--- Javascript/Juego Avatar/js/avatar.js
+++ Javascript/Juego Avatar/js/avatar.js
@@ -1,6 +1,12 @@
-let botonPersonajeJugador = document.getElementById("boton-seleccionar");
+function iniciarJuego() {
+  let botonPersonajeJugador = document.getElementById("boton-seleccionar");
 
-botonPersonajeJugador.addEventListener("click", seleccionarPersonajeJugador);
+  botonPersonajeJugador.addEventListener("click", seleccionarPersonajeJugador);
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function seleccionarPersonajeJugador() {
   let inputZuko = document.getElementById("zuko");
@@ -8,15 +14,36 @@ function seleccionarPersonajeJugador() {
   let inputKatara = document.getElementById("katara");
   let inputToph = document.getElementById("toph");
 
+  let spanPersonajeJugador = document.getElementById("personaje-jugador");
+
   if (inputZuko.checked) {
-    alert("Has seleccionado a Zuko");
+    spanPersonajeJugador.innerHTML = "Zuko";
   } else if (inputAang.checked) {
-    alert("Has seleccionado a Aang");
+    spanPersonajeJugador.innerHTML = "Aang";
   } else if (inputKatara.checked) {
-    alert("Has seleccionado a Katara");
+    spanPersonajeJugador.innerHTML = "Katara";
   } else if (inputToph.checked) {
-    alert("Has seleccionado a Toph");
+    spanPersonajeJugador.innerHTML = "Toph";
   } else {
     alert("Selecciona un personaje para jugar");
   }
+
+  seleccionarPersonajeEnemigo();
 }
+
+function seleccionarPersonajeEnemigo() {
+  let spanPersonajeEnemigo = document.getElementById("personaje-enemigo");
+  let personajeAleatorio = aleatorio(1, 4);
+
+  if (personajeAleatorio == 1) {
+    spanPersonajeEnemigo.innerHTML = "Zuko";
+  } else if (personajeAleatorio == 2) {
+    spanPersonajeEnemigo.innerHTML = "Aang";
+  } else if (personajeAleatorio == 3) {
+    spanPersonajeEnemigo.innerHTML = "Katara";
+  } else if (personajeAleatorio == 4) {
+    spanPersonajeEnemigo.innerHTML = "Toph";
+  }
+}
+
+window.addEventListener("load", iniciarJuego);
