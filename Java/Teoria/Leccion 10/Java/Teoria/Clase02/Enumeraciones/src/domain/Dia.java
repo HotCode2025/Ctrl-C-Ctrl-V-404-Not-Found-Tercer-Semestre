cc252d80e1d983a5845233f75400c69e98ae4af8
rocio2102 <rocioluceroal@gmail.com>
2026-06-20 01:48:42 -0300

    Borro carpeta duplicada

--- Java/Teoria/Leccion 10/Java/Teoria/Clase02/Enumeraciones/src/domain/Dia.java
+++
@@ -1,5 +0,0 @@
-package domain;
-
-public enum Dia {
-    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
-}
--- Java/Teoria/Leccion 10/Python/clase 4/README.md
+++
@@ -1,1 +0,0 @@
-Esta carpeta contiene las clases 4, 5 y 6 de Python del 3er semestre
